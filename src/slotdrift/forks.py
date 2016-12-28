f3951f56b5c32c2b01d113d9ea2848c973ac7bfe
Ryft <10768614+propavingk@users.noreply.github.com>
2016-12-28 09:17:00 -0500

    build: python

---
+++ src/slotdrift/forks.py
@@ -0,0 +1,11 @@
+"""Fork detection: duplicate slots, canonical ancestry and orphan segments.
+
+The cluster can produce more than one block for the same slot, and roughly one
+block in twenty never reaches finalization. This module makes both facts
+visible in an export.
+
+Canonical chain rule (deterministic, documented in docs/FORMAT.md):
+1. The tip is the record with the strongest commitment; ties break to the
+   highest slot, then to the lexicographically smallest blockhash.
+2. From the tip, walk parent links through the best record per slot.
+3. Every produced record outside that ancestry is orphaned and is grouped into
