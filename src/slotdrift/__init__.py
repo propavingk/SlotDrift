2a481708c70c4e1aec65b7671ed356e850557a2c
Ryft <10768614+propavingk@users.noreply.github.com>
2017-03-28 10:34:00 -0500

    build: cli

--- src/slotdrift/__init__.py
+++ src/slotdrift/__init__.py
@@ -1,2 +1,4 @@
 """SlotDrift: slot continuity and fork analysis for captured Solana slot records.
 
+The package is offline and deterministic. It reads an export of slot records
+(one JSON object per line), validates it, and reports continuity gaps, skipped
