1524bed54d642efc574c88648090f495788804ff
Ryft <10768614+propavingk@users.noreply.github.com>
2017-04-07 14:17:00 -0500

    feat: add parent

--- src/slotdrift/__init__.py
+++ src/slotdrift/__init__.py
@@ -3,3 +3,4 @@
 The package is offline and deterministic. It reads an export of slot records
 (one JSON object per line), validates it, and reports continuity gaps, skipped
 slots, duplicate-slot forks, orphaned branches and per-leader skip rates.
+"""
