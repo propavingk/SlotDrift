79c31fa0eea716eaf257489110b09620f6b387c2
Ryft <10768614+propavingk@users.noreply.github.com>
2018-01-18 13:15:00 -0500

    fix: python

--- src/slotdrift/cli.py
+++ src/slotdrift/cli.py
@@ -10,3 +10,14 @@ The CLI never touches the network and never writes files unless asked.
 """
 from __future__ import annotations
 
+import argparse
+import json
+import sys
+from pathlib import Path
+
+from . import __version__
+from .continuity import analyze_continuity
+from .forks import find_forks
+from .model import parse_file
+from .report import Analysis, render_json, render_text
+
