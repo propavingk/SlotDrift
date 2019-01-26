78575cad0954352f4b0c54bd68c17ddf60a522b2
Diaz90500 <309327722+Diaz90500@users.noreply.github.com>
2019-01-26 13:03:00 -0500

    build: window

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,3 +6,12 @@ import io
 import json
 import unittest
 from pathlib import Path
+
+from slotdrift.cli import main
+
+SAMPLES = Path(__file__).resolve().parents[1] / "samples"
+
+
+def run(args: list[str]) -> tuple[int, str]:
+    buffer = io.StringIO()
+    with contextlib.redirect_stdout(buffer):
