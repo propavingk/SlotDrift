5952349601ec571004b10778180c3df548ec8bb7
Ryft <10768614+propavingk@users.noreply.github.com>
2019-01-26 13:06:00 -0500

    feat: parent

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,3 +15,12 @@ SAMPLES = Path(__file__).resolve().parents[1] / "samples"
 def run(args: list[str]) -> tuple[int, str]:
     buffer = io.StringIO()
     with contextlib.redirect_stdout(buffer):
+        code = main(args)
+    return code, buffer.getvalue()
+
+
+class ExitCodeTests(unittest.TestCase):
+    def test_clean_window_exits_zero(self):
+        code, output = run(["analyze", str(SAMPLES / "clean-window.jsonl")])
+        self.assertEqual(code, 0)
+        self.assertIn("FINDINGS: 0", output)
