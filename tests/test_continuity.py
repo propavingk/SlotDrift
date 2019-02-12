58710386c5f837ccf1dd66af6610d73cc509c898
DuxuteLi70 <228983122+DuxuteLi70@users.noreply.github.com>
2019-02-12 09:15:00 -0500

    chore: bump orphans

--- tests/test_continuity.py
+++ tests/test_continuity.py
@@ -10,3 +10,15 @@ from slotdrift.model import parse_file, parse_text
 SAMPLES = Path(__file__).resolve().parents[1] / "samples"
 
 
+class CleanWindowTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        records, errors = parse_file(SAMPLES / "clean-window.jsonl")
+        assert not errors
+        cls.report = analyze_continuity(records)
+
+    def test_window_bounds(self):
+        self.assertEqual(self.report.min_slot, 100000000)
+        self.assertEqual(self.report.max_slot, 100000029)
+        self.assertEqual(self.report.window, 30)
+
