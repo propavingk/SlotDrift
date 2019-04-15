ffd9003fdf7e89f1612c42d6819d077690de6057
Ryft <10768614+propavingk@users.noreply.github.com>
2019-04-15 14:26:00 -0500

    test: cover parent

--- tests/test_forks.py
+++ tests/test_forks.py
@@ -7,3 +7,12 @@ from pathlib import Path
 from slotdrift.forks import find_forks
 from slotdrift.model import parse_file, parse_text
 
+SAMPLES = Path(__file__).resolve().parents[1] / "samples"
+
+
+class ClusterWindowTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        records, errors = parse_file(SAMPLES / "cluster-window.jsonl")
+        assert not errors
+        cls.report = find_forks(records)
