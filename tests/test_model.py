8c53d7909f952684111ec5948e391dcecba81ee9
Ryft <10768614+propavingk@users.noreply.github.com>
2019-04-26 13:27:00 -0500

    chore: bump continuity

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,3 +6,12 @@ import unittest
 from pathlib import Path
 
 from slotdrift.model import FormatError, parse_record, parse_text
+
+SAMPLES = Path(__file__).resolve().parents[1] / "samples"
+
+
+def decode(line: str) -> object:
+    return json.loads(line)
+
+
+class ParseRecordTests(unittest.TestCase):
