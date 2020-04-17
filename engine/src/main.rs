225d547fae5f1bb8dfc76a5e6fdb09020a5a960b
Ryft <10768614+propavingk@users.noreply.github.com>
2020-04-17 13:39:00 -0500

    fix: continuity

---
+++ engine/src/main.rs
@@ -0,0 +1,7 @@
+//! Command line front end for the Rust engine.
+//!
+//! Prints key: value lines, one per line, so the parity script can compare
+//! the numbers against the Python implementation without sharing any code.
+//!
+//! Exit codes: 0 clean, 1 findings, 2 usage or input error.
+
