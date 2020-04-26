511c5d24ccfd095e09df302870924cf58d53772d
Ryft <10768614+propavingk@users.noreply.github.com>
2020-04-26 09:42:00 -0500

    chore: report

--- engine/tests/engine.rs
+++ engine/tests/engine.rs
@@ -7,3 +7,12 @@ fn sample(name: &str) -> String {
     path.pop();
     path.push("samples");
     path.push(name);
+    std::fs::read_to_string(path).expect("fixture readable")
+}
+
+#[test]
+fn clean_window_has_no_findings() {
+    let (records, errors) = slotdrift_engine::parse_text(&sample("clean-window.jsonl"));
+    assert_eq!(records.len(), 30);
+    assert!(errors.is_empty());
+    let continuity = slotdrift_engine::analyze_continuity(&records);
