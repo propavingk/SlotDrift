f707fc3b45f3967b24773896108e6c05f3ed7010
Ryft <10768614+propavingk@users.noreply.github.com>
2020-04-26 09:54:00 -0500

    refactor: python

--- engine/tests/engine.rs
+++ engine/tests/engine.rs
@@ -16,3 +16,13 @@ fn clean_window_has_no_findings() {
     assert_eq!(records.len(), 30);
     assert!(errors.is_empty());
     let continuity = slotdrift_engine::analyze_continuity(&records);
+    assert_eq!(continuity.missing.len(), 0);
+    assert_eq!(continuity.parent_anomalies.len(), 0);
+    assert_eq!(continuity.skipped, vec![100_000_010, 100_000_011]);
+    let forks = slotdrift_engine::find_forks(&records);
+    assert_eq!(forks.orphan_count, 0);
+    assert!(forks.orphan_segments.is_empty());
+}
+
+#[test]
+fn cluster_window_reports_the_designed_findings() {
